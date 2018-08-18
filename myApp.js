c928054c6a62089ced01e60cec4707fbad9d40f2
Siva Karthik R <sivakarthik51@gmail.com>
2018-08-18 23:40:04 +0530

    Changing default port

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 
 var express = require('express');
 var path = require('path');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 var app = express();
 
 var listener = app.listen(port,function(){
